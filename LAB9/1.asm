fee0f6b678c5dd9dd3f63d59c94e78c927ef4087
Alapati-Saibaba <saibabaalapatiofficial@gmail.com>
2021-04-16 13:33:49 +0530

    LAB9

---
+++ LAB9/1.asm
@@ -0,0 +1,25 @@
+[12:59 PM, 4/16/2021] Saibaba-Alapati: plz
+[1:06 PM, 4/16/2021] Gayatri: org 100h
+
+#start=stepper_motor.exe#
+jmp start
+datain db 0000_0011b
+       db 0000_0110b
+       db 0000_1100b
+       db 0000_1001b
+       
+       START: MOV BX, offset datain
+       MOV SI,0h
+       NEXT_STEP:
+       WAIT: IN AL,07H
+       TEST AL,10000000b
+       JZ WAIT
+       MOV AL,[BX][SI]
+       OUT 7,AL
+       INC SI
+       CMP SI,4
+       JC NEXT_STEP
+       MOV SI,0
+       JMP NEXT_STEP
+
+ret
